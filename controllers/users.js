2ea94af8372a13f74e1a951f313a74fd6eebf5bb
MellivoraCapens <mertcagrikaptan@gmail.com>
2023-07-16 17:29:11 +0300

    server.js created

--- controllers/users.js
+++ controllers/users.js
@@ -9,10 +9,22 @@ exports.createUser = async (req, res, next) => {
             data: user
         })
     } catch(error) {
-        console.error('failed', error);
+        console.error('Failed', error);
         res.status(401).json({
             success: false,
             message: error.message
         })
     };
+}
+
+exports.getUser = async (req, res) => {
+    try {
+        
+    } catch(error) {
+        console.error('Not Found', error);
+        res.status(404).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
