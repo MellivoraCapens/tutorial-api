a8fb41f5801075995a69d630a2a24dd3dc9fa9f3
MellivoraCapens <mertcagrikaptan@gmail.com>
2023-07-20 04:53:33 +0300

    CRUD functionality completed

--- controllers/users.js
+++ controllers/users.js
@@ -3,22 +3,61 @@ const User = require('../models/user');
 // @desc    show all users
 // @route   GET /api/v1/users
 // @access  public
-exports.getUsers = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all users' });
+exports.getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({
+            success: true,
+            count: users.length,
+            data: users
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    show single user
 // @route   GET /api/v1/users/:id
 // @access  public
-exports.getUser = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show user ${req.params.id}` });
+exports.getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if(!user) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    update user
 // @route   PUT /api/v1/users/:id
 // @access  private
-exports.updateUser = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update user ${req.params.id}` });
+exports.updateUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+    
+        if(!user) {
+            res.status(400).json({ success: false });
+        }
+    
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    create user
@@ -40,6 +79,19 @@ exports.createUser = async (req, res, next) => {
 // @desc    delete user
 // @route   DELETE /api/v1/users/:id
 // @access  private
-exports.deleteUser = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete user ${req.params.id}` });
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if(!user) {
+            res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
     PORT, 
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-    );
+);
 
 process.on('unhandleRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
