70461ed75c751371c7709ac6348859a4330d0804
MellivoraCapens <mertcagrikaptan@gmail.com>
2023-07-27 18:47:22 +0300

    user CRUD updated for only admin using

--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,20 @@ const {
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin'));
 
 router
     .route('/')
     .get(getUsers)
-    .post(protect, createUser);
+    .post(createUser);
 
 router
     .route('/:id')
     .get(getUser)
-    .put(protect, updateUser)
-    .delete(protect, deleteUser);
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
